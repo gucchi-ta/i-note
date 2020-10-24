9f126f59fd10b6040a1e48fe352cda75b777ce38
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-24 16:40:35 +0900

    ユーザー管理　モデル

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # アソシエーション
+  has_many :posts, dependent: :destroy
+  
+  # バリデーション
+    # 空のデータを保存できないようにする
+    validates :nickname, :birthday, presence: true
+    # passwordのバリデーション
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: PASSWORD_REGEX }, length: { minimum: 6 }
+  # /バリデーション
+end
