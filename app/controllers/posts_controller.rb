6486f7d85cdbde943dec138d3b0fc0b0b0113a43
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-24 16:39:56 +0900

    ユーザー管理　コントローラー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
     @posts = Post.all
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
