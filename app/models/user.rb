9f0c7a3b514b630aa6ec8bb7ee3625c7144d2785
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-24 18:37:22 +0900

    rubocop 実行/修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
   # アソシエーション
   has_many :posts, dependent: :destroy
-  
+
   # バリデーション
-    # 空のデータを保存できないようにする
-    validates :nickname, :birthday, presence: true
-    # passwordのバリデーション
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates :password, format: { with: PASSWORD_REGEX }, length: { minimum: 8, maximum: 20}
+  # 空のデータを保存できないようにする
+  validates :nickname, :birthday, presence: true
+  # passwordのバリデーション
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }, length: { minimum: 8, maximum: 20 }
   # /バリデーション
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,68 +8,68 @@ RSpec.describe User, type: :model do
 
     describe 'ユーザー登録機能' do
       context 'ユーザー登録機能がうまくいく時' do
-        it "全ての情報を記述した場合は登録できる" do
+        it '全ての情報を記述した場合は登録できる' do
           expect(@user).to be_valid
         end
-        it "passwordが8~20文字の英数字なら登録できる" do
+        it 'passwordが8~20文字の英数字なら登録できる' do
           @user.password = '1111aaaa'
           @user.password_confirmation = '1111aaaa'
           expect(@user).to be_valid
         end
       end
       context 'ユーザー登録機能がうまくいかない時' do
-        it "nicknameが空だと保存できない" do
+        it 'nicknameが空だと保存できない' do
           @user.nickname = nil
           @user.valid?
           expect(@user.errors.full_messages).to include("Nickname can't be blank")
         end
-        it "emailが空だと保存できない" do
+        it 'emailが空だと保存できない' do
           @user.email = nil
           @user.valid?
           expect(@user.errors.full_messages).to include("Email can't be blank")
         end
-        it "passwordが空だと保存できない" do
+        it 'passwordが空だと保存できない' do
           @user.password = nil
           @user.valid?
           expect(@user.errors.full_messages).to include("Password can't be blank")
         end
-        it "passwordがあってもpassword_confirmationが空だと保存できない" do
+        it 'passwordがあってもpassword_confirmationが空だと保存できない' do
           @user.password = '1111aaaa'
           @user.password_confirmation = ''
           @user.valid?
           expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
         end
-        it "passwordとpassword_confirmationの値が異なっていると保存できない" do
+        it 'passwordとpassword_confirmationの値が異なっていると保存できない' do
           @user.password = '1111aaaa'
           @user.password_confirmation = '2222bbbb'
           @user.valid?
           expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
         end
-        it "passwordが8文字以下だと保存できない" do
+        it 'passwordが8文字以下だと保存できない' do
           @user.password = '1111aaa'
           @user.password_confirmation = '1111aaa'
           @user.valid?
-          expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+          expect(@user.errors.full_messages).to include('Password is too short (minimum is 8 characters)')
         end
-        it "passwordが20文字以上だと保存できない" do
+        it 'passwordが20文字以上だと保存できない' do
           @user.password = '11111111111aaaaaaaaaa'
           @user.password_confirmation = '11111111111aaaaaaaaaa'
           @user.valid?
-          expect(@user.errors.full_messages).to include("Password is too long (maximum is 20 characters)")
+          expect(@user.errors.full_messages).to include('Password is too long (maximum is 20 characters)')
         end
-        it "passwordが英字のみだと保存できない" do
+        it 'passwordが英字のみだと保存できない' do
           @user.password = 'aaaaaaaa'
           @user.password_confirmation = 'aaaaaaaa'
           @user.valid?
-          expect(@user.errors.full_messages).to include("Password is invalid")
+          expect(@user.errors.full_messages).to include('Password is invalid')
         end
-        it "passwordが数字のみだと保存できない" do
+        it 'passwordが数字のみだと保存できない' do
           @user.password = '11111111'
           @user.password_confirmation = '11111111'
           @user.valid?
-          expect(@user.errors.full_messages).to include("Password is invalid")
+          expect(@user.errors.full_messages).to include('Password is invalid')
         end
-        it "birthdatが空だと保存できない" do
+        it 'birthdatが空だと保存できない' do
           @user.birthday = nil
           @user.valid?
           expect(@user.errors.full_messages).to include("Birthday can't be blank")
@@ -77,5 +77,4 @@ RSpec.describe User, type: :model do
       end
     end
   end
-
 end
